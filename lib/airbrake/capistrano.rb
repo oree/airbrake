2daf9a5109565ef049ef00e15aa1dbe42bf8a3a2
Eero Raun <eero.raun@zoined.com>
2014-10-17 13:35:28 +0300

    Ignore RACK_ENV if it is empty (capistrano deploy).

--- lib/airbrake/capistrano.rb
+++ lib/airbrake/capistrano.rb
@@ -71,9 +71,9 @@ module Airbrake
               - Run remotely so we use remote API keys, environment, etc.
           DESC
           task :deploy, :except => { :no_release => true } do
-            rack_env = fetch(:rack_env, "production")
-            rails_env = fetch(:rails_env, "production")
-            airbrake_env = fetch(:airbrake_env, rack_env || rails_env)
+            rack_env = fetch(:rack_env, nil)
+            rails_env = fetch(:rails_env, nil)
+            airbrake_env = fetch(:airbrake_env, rack_env || rails_env || "production")
             local_user = ENV['USER'] || ENV['USERNAME']
             executable = RUBY_PLATFORM.downcase.include?('mswin') ? fetch(:rake, 'rake.bat') : fetch(:rake, 'bundle exec rake ')
             directory = configuration.release_path
